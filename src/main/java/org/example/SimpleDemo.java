a986bb2e49fab326db4947aef064190eb419c08e
DavidK97 <david.700@live.dk>
2025-02-06 14:50:15 +0100

    refactored to make it multithreaded, to take multiple clients

--- src/main/java/org/example/SimpleDemo.java
+++ src/main/java/org/example/SimpleDemo.java
@@ -7,32 +7,26 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import static java.lang.System.out;
+
 public class SimpleDemo {
     private ServerSocket server;
-    private Socket clientHandler;
-    private PrintWriter out;
-    private BufferedReader in;
+
 
     public void start () {
         try {
             server = new ServerSocket(8080);
-            clientHandler = server.accept();
-            out = new PrintWriter(clientHandler.getOutputStream(), true); //Sekvens af 0'er og 1-taller der sendes
-            in = new BufferedReader(new InputStreamReader(clientHandler.getInputStream()));
-            out.println("Connection established");
-            String inputLine;
-            while ((inputLine = in.readLine()) !=  null) { //Tjekker om der kommer noget ind fra klienten
-                System.out.println("Message from client: " + inputLine);
-                if ("Exit".equals(inputLine)){ //Breaks the while loop if the client enters "Exit"
-                    out.println("The server is shutting down");
-                    break;
-                }
+
+
+        while (true) {
+                Socket socket = server.accept();
+                Runnable clientHandler = new ClientHandler(socket);
+                new Thread (clientHandler).start();
             }
-
-        } catch (IOException ex) {
-            //TODO: Fjern stacktrace inden produktion
-            ex.printStackTrace();
-
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+/*
         } finally { //Bliver altid udført
             try {
                 stop();
@@ -40,14 +34,49 @@ public class SimpleDemo {
                 ex.printStackTrace();
             }
         }
-    }
 
+           */
+    }
+    /*
     private void stop () throws IOException {
         in.close();
         out.close();
-        clientHandler.close();
+        clientSocket.close();
         server.close();
     }
+
+     */
+
+    private static class ClientHandler implements Runnable {
+        private Socket clientSocket;
+
+        public ClientHandler (Socket socket) {
+            this.clientSocket = socket;
+        }
+
+        @Override
+        public void run(){
+            try {
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true); //Sekvens af 0'er og 1-taller der sendes
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            out.println("Connection established");
+            String inputLine;
+            while ((inputLine = in.readLine()) !=  null) { //Tjekker om der kommer noget ind fra klienten
+                System.out.println("Message from client: " + inputLine); //Printer i konsollen
+                if ("Exit".equals(inputLine)) { //Breaks the while loop if the client enters "Exit"
+                    out.println("The server is shutting down");
+                    in.close();
+                    out.close();
+                    clientSocket.close();
+                    break;
+                }
+            }
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+        }
+    }
+
 
     public static void main(String[] args) {
         new SimpleDemo().start();
